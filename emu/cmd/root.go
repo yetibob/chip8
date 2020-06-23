a6f5188e59624fcbc79157abfe0cdd8eba8ef8b5
Wickie Lee <wickie.leejr@gmail.com>
2020-06-23 00:40:47 -0400

    time to start the actual emulator

---
+++ emu/cmd/root.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"io/ioutil"
+
+	"github.com/spf13/cobra"
+	"github.com/yetibob/chip8/emu/op"
+)
+
+var (
+	// Used for flags.
+	rootCmd = &cobra.Command{
+		Use:   "regen",
+		Short: "regen runs through a chip8 program and spits out assembly ",
+		Run: func(cmd *cobra.Command, args []string) {
+			// read rom into buffer
+			romFile, err := cmd.PersistentFlags().GetString("rom")
+			panicErr(err)
+
+			rom, err := ioutil.ReadFile(romFile)
+			panicErr(err)
+
+			pc := 0
+			for {
+				if pc >= len(rom) {
+					break
+				}
+				pc = op.HandleOp(rom[pc:pc+2], pc)
+			}
+		},
+	}
+)
+
+func panicErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Execute t
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringP("rom", "r", "", "rom file to regen")
+	rootCmd.MarkPersistentFlagRequired("rom")
+}
+
+func initConfig() {
+	return
+}
