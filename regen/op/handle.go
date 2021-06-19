f0555759247ef4497cde2ba765ec6ab12cc44370
Wickie Lee <wickie.leejr@protonmail.com>
2021-06-19 16:36:29 -0400

    fix regen

--- regen/op/handle.go
+++ regen/op/handle.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 )
 
+var opbytes = 2
+
 func HandleOp(buf []byte, pc int) int {
-	// Go ahead and advance pc by 2. This will be overridden by any opcodes that exclusive set pc
+	// Go ahead and advance pc by 2. This will be overridden by any opcodes that exclusively set pc
 	// prior to returning from the function
 	pc += 2
 
 	// Chip 8 instruction are technically 2 bytes long however they are read in via 1 byte increments
 	// Therefore we need to combine them into a single 16 bit integer
-	var op uint16
-	op = uint16(buf[1])<<8 | uint16(buf[0])
+	var op uint16 = uint16(buf[1])<<8 | uint16(buf[0])
 
 	addr := op & 0xFFF
-	n := op & 0xF
+	// n := op & 0xF
 	x := op & 0x0F00
 	y := op & 0x00F0
 	kk := op & 0x00FF
@@ -29,39 +30,39 @@ func HandleOp(buf []byte, pc int) int {
 			fmt.Printf("RET\n")
 		}
 	case 0x1:
-		fmt.Println("JMP addr: 0x%X", addr)
+		fmt.Printf("JMP addr: 0x%X\n", addr)
 	case 0x2:
-		fmt.Println("CALL addr: 0x%X", addr)
+		fmt.Printf("CALL addr: 0x%X\n", addr)
 	case 0x3:
-		fmt.Println("SE Vx, kk: 0x%X 0x%X", x, kk)
+		fmt.Printf("SE Vx, kk: 0x%X 0x%X\n", x, kk)
 	case 0x4:
-		fmt.Println("SNE Vx, kk: 0x%X 0x%X", x, kk)
+		fmt.Printf("SNE Vx, kk: 0x%X 0x%X\n", x, kk)
 	case 0x5:
-		fmt.Println("SE Vx, Vy: 0x%X 0x%X", x, y)
+		fmt.Printf("SE Vx, Vy: 0x%X 0x%X\n", x, y)
 	case 0x6:
-		fmt.Println("LD Vx, kk: 0x%X 0x%X", x, kk)
+		fmt.Printf("LD Vx, kk: 0x%X 0x%X\n", x, kk)
 	case 0x7:
-		fmt.Println("ADD Vx, kk: 0x%X 0x%X", x, kk)
+		fmt.Printf("ADD Vx, kk: 0x%X 0x%X\n", x, kk)
 	case 0x8:
 		switch op & 0xF {
 		case 0x0:
-			fmt.Println("LD Vx, Vy")
+			fmt.Printf("LD Vx, Vy\n")
 		case 0x1:
-			fmt.Println("OR Vx, Vy")
+			fmt.Printf("OR Vx, Vy\n")
 		case 0x2:
-			fmt.Println("AND Vx, Vy")
+			fmt.Printf("AND Vx, Vy\n")
 		case 0x3:
-			fmt.Println("XOR Vx, Vy")
+			fmt.Printf("XOR Vx, Vy\n")
 		case 0x4:
-			fmt.Println("ADD Vx, Vy")
+			fmt.Printf("ADD Vx, Vy\n")
 		case 0x5:
-			fmt.Println("SUB Vx, Vy")
+			fmt.Printf("SUB Vx, Vy\n")
 		case 0x6:
-			fmt.Println("SHR Vx, {, Vy}")
+			fmt.Printf("SHR Vx, {, Vy}\n")
 		case 0x7:
-			fmt.Println("SUBN Vx, Vy")
+			fmt.Printf("SUBN Vx, Vy\n")
 		case 0xE:
-			fmt.Println("SHL Vx {, Vy}")
+			fmt.Printf("SHL Vx {, Vy}\n")
 		}
 	case 0x9:
 		fmt.Println("SNE Vx, Vy")
